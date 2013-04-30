9064fd9349a5e764de7e2baf4a6cd4f4ddb1cb9a
Caleb Simpson <caleb.simpson@jadedpixel.com>
2013-04-30 16:15:32 -0400

    Extract common code (grid and vector2).

---
+++ lib/grid.rb
@@ -0,0 +1,80 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class Grid
+  def self.GRID_SIZE
+    19
+  end
+
+  def initialize
+    @grid = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+    (0..Grid.GRID_SIZE).each do |y|
+      (0..Grid.GRID_SIZE).each do |x|
+        @grid[x][y] = @grid[x][y] == 1 ? true : false
+      end
+    end
+  end
+
+  def player=(player)
+    @player = player
+  end
+
+  def draw
+    result = ""
+    (0..Grid.GRID_SIZE).each do |y|
+      (0..Grid.GRID_SIZE).each do |x|
+        position = Vector2.new(x, y)
+        if @player.position == position
+          result += "## "
+        elsif @player.target == position
+          result += "XX "
+        elsif @player.path.include? position
+          result += "oo "
+        else
+          found = nil
+          @player.open.each do |node|
+            if node.position == position
+              found = ".. "
+            end
+          end
+          @player.closed.each do |node|
+            if node.position == position
+              found = "** "
+            end
+          end
+          if found.nil?
+            result += blocked?(position) ? "██ " : "   "
+          else
+            result += found
+          end
+        end
+      end
+      result += "\n"
+    end
+
+    puts result
+  end
+
+  def blocked?(position)
+    @grid[position.y][position.x]
+  end
+end
---
+++ lib/vector2.rb
@@ -0,0 +1,23 @@
+class Vector2
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def ==(other)
+    @x == other.x && @y == other.y
+  end
+
+  def direction(other)
+    return :diagonal if other.x != x && other.y != y
+    return :horizontal if other.x == x && other.y != y
+    return :vertical if other.x != x && other.y == y
+    return :none
+  end
+
+  def to_s
+    "(#{@x}, #{@y})"
+  end
+end
